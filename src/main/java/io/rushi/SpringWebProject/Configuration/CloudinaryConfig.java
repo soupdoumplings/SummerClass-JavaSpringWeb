735e0b6f751ed6bfab0ba4621e52ffb0dcd7364a
soupdoumplings <mountdihverest@gmail.com>
2026-07-28 11:57:58 +0545

    Added CloudinaryConfig

--- src/main/java/io/rushi/SpringWebProject/Configuration/CloudinaryConfig.java
+++ src/main/java/io/rushi/SpringWebProject/Configuration/CloudinaryConfig.java
@@ -1,14 +1,24 @@
 package io.rushi.SpringWebProject.Configuration;
 
 import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 
 @Configuration
 public class CloudinaryConfig {
 
+    @Bean
     public Cloudinary Cloudinary() {
 
-        return new Cloudinary();
+        return new Cloudinary(ObjectUtils.asMap(
+
+                "cloud_name","glwfymp3",
+                "api_key","428393921153524",
+                "api_secret","I6aj62hM9DfLSBKkRP3UnBOx3V4",
+                "secure","true"
+        ));
 
     }
 }
